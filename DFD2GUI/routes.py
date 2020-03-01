1079ba99a434fb1d4dbce0f134f6d8eadf73ee92
daryfauzan <daryfauzan@gmail.com>
2020-03-01 08:46:22 +0700

    Add login feature

--- DFD2GUI/routes.py
+++ DFD2GUI/routes.py
@@ -2,11 +2,24 @@ from flask import render_template, url_for, flash, redirect
 from DFD2GUI import app, db
 from DFD2GUI.forms import RegistrationForm, LoginForm
 from DFD2GUI.models import User, Project
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/",  methods=['POST', 'GET'])
 @app.route("/login",  methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    print("email:",form.email.data)
+    print("password:",form.password.data)
+    print()
+    print(form.email.errors)
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        print(form.email.data, form.password.data)
+        if user and form.password.data == user.password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Your email or password is wrong", "danger")
     return render_template('login.html', title="Login DFD2GUI", form=form)
 
 @app.route("/register", methods=['POST', 'GET'])
@@ -19,3 +32,7 @@ def register():
         flash("Your account has been created", "success")
         return redirect(url_for('login'))
     return render_template('register.html', title="Register DFD2GUI", form=form)
+
+@app.route("/dashboard")
+def dashboard():
+    return "<h1>Dasahboard</h1>"
