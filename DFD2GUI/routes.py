7d5785d6110b1dbbfe2eb0e7107e0fdbdeb4c8ed
Dary Fauzan <daryfauzan@pop-os.localdomain>
2020-03-05 17:15:04 +0700

    Update dashboard. Add relevant table and fix navbar

--- DFD2GUI/routes.py
+++ DFD2GUI/routes.py
@@ -4,6 +4,7 @@ from DFD2GUI.forms import RegistrationForm, LoginForm
 from DFD2GUI.models import User, Project
 from flask_login import login_user, current_user, logout_user, login_required
 
+active_link = {'dashboard':'', 'project-list':'', 'upload-dfd':''}
 
 @app.route("/login",  methods=['POST', 'GET'])
 @app.route("/",  methods=['POST', 'GET'])
@@ -24,7 +25,7 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash("Your email or password is wrong", "danger")
-    return render_template('login.html', title="Login DFD2GUI", form=form)
+    return render_template('login.html', title="Login", form=form)
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
@@ -37,13 +38,25 @@ def register():
         db.session.commit()
         flash("Your account has been created", "success")
         return redirect(url_for('login'))
-    return render_template('register.html', title="Register DFD2GUI", form=form)
-
+    return render_template('register.html', title="Register", form=form)
 
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    active_link['dashboard'] = 'active'
+    return render_template('dashboard.html', title="Dashboard", active_link=active_link)
+
+@app.route("/project-list")
+@login_required
+def project_list():
+    return "Ini list project"
+    # return render_template('dashboard.html', title="Dashboard")
+
+@app.route("/upload-dfd")
+@login_required
+def upload_dfd():
+    return "Ini upload DFD"
+    # return render_template('dashboard.html', title="Dashboard")
 
 @app.route("/logout")
 def logout():
