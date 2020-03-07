b1ee42914f36a826ef2d4f7bdbba119c8f60b0f2
daryfauzan <daryfauzan@gmail.com>
2020-03-07 08:01:37 +0700

    Add registered user project folder feature

--- DFD2GUI/routes.py
+++ DFD2GUI/routes.py
@@ -3,7 +3,9 @@ from DFD2GUI import app, db
 from DFD2GUI.forms import RegistrationForm, LoginForm
 from DFD2GUI.models import User, Project
 from flask_login import login_user, current_user, logout_user, login_required
+import os
 
+@app.route("/", methods=["POST", "GET"])
 @app.route("/login",  methods=['POST', 'GET'])
 def login():
     form = LoginForm()
@@ -26,15 +28,23 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        # Add user to database
         user = User(name=form.name.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        # Add user's folder
+        directory = form.email.data
+        parent_dir = "DFD2GUI/user_project"
+        path = os.path.join(parent_dir, directory)
+        os.mkdir(path)
+
+        # Redirect to login
         flash("Your account has been created", "success")
         return redirect(url_for('login'))
     return render_template('register.html', title="Register DFD2GUI", form=form)
 
 
-@app.route("/")
 @app.route("/dashboard")
 @login_required
 def dashboard():
